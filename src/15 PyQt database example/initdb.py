e34f1c13e81fc72af81b88ade5517bc70d4db33e
Mike Pritchard <mikey.pritchard@gmail.com>
2021-08-30 23:29:07 -0400

    Working on the SQL stuff

--- src/15 PyQt database example/initdb.py
+++ src/15 PyQt database example/initdb.py
@@ -2,8 +2,34 @@ import sqlite3
 connection = sqlite3.connect("projects.db")
 cursor = connection.cursor()
 cursor.execute("""
-    CREATE TABLE projects
-    (url TEXT, descr TEXT, income INTEGER)
+    CREATE TABLE owners
+    (   ownerID INTEGER PRIMARY KEY, fname TEXT, lname TEXT, addr1 TEXT, addr2 TEXT, city TEXT, state TEXT, zip TEXT, 
+        country TEXT, notes TEXT)
+""")
+cursor.execute("""
+    CREATE TABLE dogs
+    (   dogID INTEGER PRIMARY KEY, callName TEXT, registeredName TEXT, notes TEXT)
+""")
+cursor.execute("""
+    CREATE TABLE owner_dogs
+    (   ownerID INTEGER, dogID INTEGER, primaryOwner INTEGER,
+            FOREIGN KEY(ownerID) REFERENCES owners(ownerID),FOREIGN KEY(dogID) REFERENCES dogs(dogID))
+""")
+cursor.execute("""
+    CREATE TABLE procedures
+    (   typeProc INTEGER, dogID INTEGER , addr2 TEXT, city TEXT, state TEXT, zip TEXT, country TEXT, notes TEXT,
+            FOREIGN KEY(dogID) REFERENCES dogs(dogID))
+""")
+cursor.execute("""
+    CREATE TABLE semen
+    (   semenID INTEGER PRIMARY KEY, dogID Integer, quality1 INTEGER, quality2 INTEGER, abnormality1_present INTEGER,
+        abnormality2_present INTEGER, abnormality3_present INTEGER, abnormality4_present INTEGER, notes TEXT,
+            FOREIGN KEY(dogID) REFERENCES dogs(dogID))
+""")
+cursor.execute("""
+    CREATE TABLE shipments
+    (   shipmentID INTEGER PRIMARY KEY, semenID INTEGER, shipAddr1 TEXT, shipAddr2 TEXT, shipCity TEXT, shipState TEXT, 
+        shipZip TEXT, shipCountry TEXT, notes TEXT)
 """)
 cursor.execute("""INSERT INTO projects VALUES 
     ('giraffes.io', 'Uber, but with giraffes', 1900),
